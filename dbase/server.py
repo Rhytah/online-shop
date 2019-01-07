31462e781dcc277220178f9db6efc81bfb892696
Rhytah <ritanamono@gmail.com>
2019-01-07 15:53:43 +0300

    Update server.py
    define connection parameters

--- dbase/server.py
+++ dbase/server.py
@@ -4,4 +4,10 @@ import datetime
 
 class DatabaseConnect:
     def __init__(self):
-        pass
+        self.credentials = dict(
+                dbname ='d4l82hlu9p9hci',
+                user = 'ptyunnnqwfcjzy',
+                password='bcc93c197c3ab85bcb3643bb85ae8bcb1fc2ec19c1aab7090e5180a38f285060',
+                host='ec2-174-129-18-247.compute-1.amazonaws.com',
+                port = 5432
+              )
